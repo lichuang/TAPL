c9a3818ecef38d208bde0a135e2c975a09cee54c
lichuang <lichuang1982@gmail.com>
2023-08-01 22:43:32 +0800

    ci

--- rust/02_untyped_lambda/src/parser.rs
+++ rust/02_untyped_lambda/src/parser.rs
@@ -1,12 +1,11 @@
 use nom::{
     branch::alt,
     bytes::complete::{tag, tag_no_case},
+    character::complete::{alpha0, alpha1},
     error::{context, VerboseError},
-    multi::many_m_n,
+    multi::many1,
     sequence::tuple,
 };
-
-use nom::character::complete::anychar;
 
 pub type IResult<I, O> = nom::IResult<I, O, VerboseError<I>>;
 
@@ -18,53 +17,62 @@ pub enum Term {
     TmApp(Box<Term>, Box<Term>),
 }
 
+fn parse_paren_term(input: &str) -> IResult<&str, Term> {
+    //println!("parse_paren_term {:?}", input);
+    context("parse_paren_term", tuple((tag("("), parse_term, tag(")"))))(input)
+        .map(|(next_input, (_, term, _))| (next_input, term))
+}
+
 fn parse_variable(input: &str) -> IResult<&str, Term> {
-    context("parse_variable", anychar)(input)
+    println!("parse_variable {:?}", input);
+    context("parse_variable", alpha1)(input)
         .map(|(next_input, res)| (next_input, Term::TmVar(res.to_string())))
 }
 
+fn parse_atom(input: &str) -> IResult<&str, Term> {
+    //println!("parse_atom {:?}", input);
+    context("parse_atom", alt((parse_variable, parse_paren_term)))(input)
+        .map(|(next_input, res)| (next_input, res))
+}
+
 fn parse_abstraction(input: &str) -> IResult<&str, Term> {
-    println!("parse_abstraction: {:?}", input);
+    //println!("parse_abstraction: {:?}", input);
     context(
         "parse_abstraction",
-        tuple((
-            tag("("),
-            tag_no_case("lambda "),
-            anychar,
-            tag("."),
-            parse_variable,
-            tag(")"),
-        )),
+        tuple((tag_no_case("lambda "), alpha0, tag("."), parse_term)),
     )(input)
-    .map(|(next_input, (_, _, param, _, body, _))| {
-        println!("param: {:?}", param);
+    .map(|(next_input, (_, param, _, body))| {
+        /*
+        println!(
+            "param: {:?}, body: {:?}, next_input: {:?}",
+            param, body, next_input
+        );
+        */
         (next_input, Term::TmAbs(param.to_string(), Box::new(body)))
     })
 }
 
 fn parse_application(input: &str) -> IResult<&str, Term> {
-    context("parse_application", many_m_n(2, 1024, parse_variable))(input).map(
-        |(next_input, vars)| {
-            println!("vars: {:?}", vars);
-
-            let mut lhs = Box::new(vars[0].clone());
-            for i in 1..vars.len() {
-                let rhs = Box::new(vars[i].clone());
-                lhs = Box::new(Term::TmApp(lhs, rhs));
-            }
-            (next_input, lhs.as_ref().clone())
-        },
-    )
+    //println!("parse_application {:?}", input);
+    context("parse_application", many1(parse_atom))(input).map(|(next_input, vars)| {
+        //println!("vars: {:?}", vars);
+        let mut lhs = Box::new(vars[0].clone());
+        for i in 1..vars.len() {
+            let rhs = Box::new(vars[i].clone());
+            lhs = Box::new(Term::TmApp(lhs, rhs));
+        }
+        (next_input, lhs.as_ref().clone())
+    })
 }
 
 fn parse_term(input: &str) -> IResult<&str, Term> {
-    println!("parse_term: {:?}", input);
+    //println!("parse_term: {:?}", input);
     context("term", alt((parse_abstraction, parse_application)))(input)
         .map(|(next_input, res)| (next_input, res))
 }
 
 pub fn parse(input: &str) -> IResult<&str, Term> {
-    println!("parse");
+    //println!("parse");
     context("parse", tuple((parse_term, tag(";"))))(input)
         .map(|(next_input, (term, _))| (next_input, term))
 }
@@ -82,12 +90,20 @@ mod tests {
                 Term::TmAbs("x".to_string(), Box::new(Term::TmVar("y".to_string())))
             ))
         );
-
         assert_eq!(
-            parse("(lambda x.(lambda y.y) z);"),
+            parse("(lambda x.(lambda y.y))z;"),
             Ok((
                 "",
-                Term::TmAbs("x".to_string(), Box::new(Term::TmVar("y".to_string())))
+                Term::TmApp(
+                    Box::new(Term::TmAbs(
+                        "x".to_string(),
+                        Box::new(Term::TmAbs(
+                            "y".to_string(),
+                            Box::new(Term::TmVar("y".to_string()))
+                        ))
+                    )),
+                    Box::new(Term::TmVar("z".to_string()))
+                )
             ))
         );
     }
